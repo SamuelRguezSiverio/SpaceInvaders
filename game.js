eb986708bff1e81204533949ec6d4b993bcc7e3b
itselenasinh <itselenagm@gmail.com>
2022-12-13 22:20:03 +0100

    game & falcon updated

--- game.js
+++ game.js
@@ -1,6 +1,19 @@
+import { MilleniumFalcon } from "./millenium_falcon.js"
+const milleniumFalcon1 = new MilleniumFalcon()
+milleniumFalcon1.update()
+milleniumFalcon1.draw()
+
+
+
+
+
+//gameLoop
+//setInterval 
+
+
+
 function Game() {
     this.score = 0
     this.milleniumFalcon = null
     this.stormtrooper = null
 }
-
--- millenium_falcon.js
+++ millenium_falcon.js
@@ -1,3 +1,24 @@
 export function MilleniumFalcon() {
-this.spacecraft = 
+this.position = {left: 135, top: 410}
+this.direction = 1
+this.domMilleniumFalcon = document.getElementById('milleniumFalcon')
 }
+
+MilleniumFalcon.prototype.update =  function () {
+    if (this.direction === 1) {
+        this.position.left += 50
+    } else if (this.direction === -1) {
+        this.position.left -= 1
+    }
+
+} 
+
+MilleniumFalcon.prototype.draw = function() {
+    this.domMilleniumFalcon.style.left = this.position.left + "px"
+    console.log(this.domMilleniumFalcon.style)
+    
+}  
+    /*left > 135 
+    return derecha
+    if left < 135
+    return izquierda*/
